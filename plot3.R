778a4ea7daae9b0628c9b7c291e68b76ff22f2d1
Jia-jie Hung <iceshadow14@gmail.com>
2017-06-22 19:00:06 +0800

    Update plot3.R
    add comment

--- plot3.R
+++ plot3.R
@@ -27,5 +27,6 @@ lines(x=data$datetime,y=data$Sub_metering_3,type="l", col="blue")
 # add annotation
 legend("topright", legend = c('Sub_metering_1', 'Sub_metering_2', 'Sub_metering_3'), lwd=1, col=c("black", "red", "blue"))
 
+# write png file
 dev.copy(png, filename="plot3.png", width=480, height=480)
 dev.off()
